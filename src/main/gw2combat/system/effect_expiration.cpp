fb498eed3f7de3d2c7b00fe0bd545bcd5dc1e1db
Manik Charan <mkchan2951@gmail.com>
2022-07-07 03:13:20 +0530

    Cleanup

--- src/main/gw2combat/system/effect_expiration.cpp
+++ src/main/gw2combat/system/effect_expiration.cpp
@@ -14,7 +14,8 @@
 namespace gw2combat::system {
 
 template <class Effect>
-void relax_multi_stack_effect(context& ctx, entt::entity entity, Effect* effect_ptr) {
+void relax_multi_stack_effect(context& ctx, entt::entity entity) {
+    Effect* effect_ptr = ctx.registry.template try_get<Effect>(entity);
     if (effect_ptr) {
         while (!effect_ptr->stacks.empty() &&
                ctx.current_tick > effect_ptr->stacks.top().end_time) {
@@ -27,7 +28,8 @@ void relax_multi_stack_effect(context& ctx, entt::entity entity, Effect* effect_
 }
 
 template <class Effect>
-void relax_single_stack_effect(context& ctx, entt::entity entity, Effect* effect_ptr) {
+void relax_single_stack_effect(context& ctx, entt::entity entity) {
+    Effect* effect_ptr = ctx.registry.template try_get<Effect>(entity);
     if (effect_ptr && ctx.current_tick > effect_ptr->stack.end_time) {
         ctx.registry.remove<Effect>(entity);
     }
@@ -37,24 +39,15 @@ void effect_expiration(context& ctx) {
     ctx.registry.view<component::character_input>().each(
         [&](const entt::entity entity, const component::character_input&) {
             // Boons
-            auto might = ctx.registry.try_get<component::might>(entity);
-            auto fury = ctx.registry.try_get<component::fury>(entity);
-            auto quickness = ctx.registry.try_get<component::quickness>(entity);
-            auto resolution = ctx.registry.try_get<component::resolution>(entity);
-            auto aegis = ctx.registry.try_get<component::aegis>(entity);
-
-            relax_multi_stack_effect<component::might>(ctx, entity, might);
-            relax_single_stack_effect<component::fury>(ctx, entity, fury);
-            relax_single_stack_effect<component::quickness>(ctx, entity, quickness);
-            relax_single_stack_effect<component::resolution>(ctx, entity, resolution);
-            relax_single_stack_effect<component::aegis>(ctx, entity, aegis);
+            relax_multi_stack_effect<component::might>(ctx, entity);
+            relax_single_stack_effect<component::fury>(ctx, entity);
+            relax_single_stack_effect<component::quickness>(ctx, entity);
+            relax_single_stack_effect<component::resolution>(ctx, entity);
+            relax_single_stack_effect<component::aegis>(ctx, entity);
 
             // Conditions
-            auto vulnerability = ctx.registry.try_get<component::vulnerability>(entity);
-            auto burning = ctx.registry.try_get<component::burning>(entity);
-
-            relax_multi_stack_effect<component::vulnerability>(ctx, entity, vulnerability);
-            relax_multi_stack_effect<component::burning>(ctx, entity, burning);
+            relax_multi_stack_effect<component::vulnerability>(ctx, entity);
+            relax_multi_stack_effect<component::burning>(ctx, entity);
         });
 }
 
